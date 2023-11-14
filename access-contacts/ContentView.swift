7646234b5df3f01602d9d5d120a7490112d8b3c2
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-11-14 09:44:22 +0300

    Initial Commit

---
+++ access-contacts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  access-contacts
+//
+//  Created by Samet Çağrı Aktepe on 14.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ access-contacts/access_contactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  access_contactsApp.swift
+//  access-contacts
+//
+//  Created by Samet Çağrı Aktepe on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct access_contactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
